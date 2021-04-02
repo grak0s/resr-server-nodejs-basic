dbeb9d74d8fd24cdf556fa18b04df9a2c53b5d2b
Alex Rodriguez <arodriguez@proyectoholdingvml.com>
2021-04-01 21:07:50 -0500

    validar roles con db

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,8 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+
+const Role = require("../models/role");
+
 const {
   usuariosGet,
   usuariosPut,
@@ -6,13 +10,34 @@ const {
   usuariosDelete,
 } = require("../controllers/usuarios");
 
+const { validarCampos } = require("../middlewares/validar-campos");
+
 const router = Router();
 
 router.get("/", usuariosGet);
 
 router.put("/:id", usuariosPut);
 
-router.post("/", usuariosPost);
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check(
+      "password",
+      "El password es obligatorio y debe contener mas de 6 letras"
+    ).isLength({ min: 6 }),
+    check("correo", "El correo no es Valido").isEmail(),
+    //check("rol", "no es un rol permitido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check("rol").custom(async (rol = "") => {
+      const existeRol = await Role.findOne({ rol });
+      if (!existeRol) {
+        throw new Error(`El rol ${rol} no esta registrado en la base de datos`);
+      }
+    }),
+    validarCampos,
+  ],
+  usuariosPost
+);
 
 router.delete("/", usuariosDelete);
 
